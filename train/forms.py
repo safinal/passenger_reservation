73b1a1b94f3e0202a5c1425a98f0223510a64f86
Ali Safinal <74741200+safinal@users.noreply.github.com>
2021-05-03 20:17:00 +0430

    change gitignore

--- train/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .cities import CITY_CHOICES
-
-
-class TripForm(forms.Form):
-    origin_city = forms.ChoiceField(choices=CITY_CHOICES)
-    destination_city = forms.ChoiceField(choices=CITY_CHOICES)
-    full_coupe = forms.BooleanField()
-
--- train/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_satisfaction_range(value):
-    if not (0 <= value <= 10):
-        raise ValidationError(f"{value} is not between 0 and 10!")
